4447385713fe32da209ee969d1582520f21f8da9
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-07 09:42:53 +0000

    feat: fetch & render products for Shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,18 @@
+import useFetchProducts from "../fetch";
 import Navbar from "./Navbar";
+import ProductList from "./ProductList";
 
 const Shop = () => {
+  const { products, error, loading } = useFetchProducts();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered.</p>;
+
   return (
     <>
       <Navbar />
       <h1 id="shop-heading">Shop</h1>
+      <ProductList title="Trending" products={products} />
     </>
   );
 };
