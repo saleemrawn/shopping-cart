fa2805ec7521e85ed52b362e478e26c99965a900
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-07 10:29:13 +0000

    feat: add App component for top-level component
    - update routes config to make App parent routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,20 @@
+import App from "./components/App.jsx";
 import Home from "./components/Home.jsx";
 import Shop from "./components/Shop.jsx";
 import Cart from "./components/Cart.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
 
 const routes = [
-  { path: "/", element: <Home />, errorElement: <ErrorPage /> },
-  { path: "/shop", element: <Shop /> },
-  { path: "/cart", element: <Cart /> },
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/shop", element: <Shop /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
 ];
 
 export default routes;
