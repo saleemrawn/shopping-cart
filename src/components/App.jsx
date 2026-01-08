23180d3198875196a52897680e6dfd612647575d
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-08 12:18:27 +0000

    feat: add cart context provider, cart state & funcs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
+import { CartProvider } from "../CartProvider";
 
 const App = () => {
-  return <Outlet />;
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  };
+
+  return (
+    <CartProvider value={{ cartItems, addToCart, removeFromCart }}>
+      <Outlet />
+    </CartProvider>
+  );
 };
 
 export default App;
