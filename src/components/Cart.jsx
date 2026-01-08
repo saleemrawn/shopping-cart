d4a6b04e05817475f077915bf0caabbea95f4738
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-08 13:54:12 +0000

    feat: render products added to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,15 @@
+import { useCart } from "../CartProvider";
 import Navbar from "./Navbar";
+import ProductList from "./ProductList";
 
 const Cart = () => {
+  const { cartItems } = useCart();
+
   return (
     <>
       <Navbar />
       <h1 id="cart-heading">Cart</h1>
+      <ProductList title="Items" products={cartItems} page="cart" />
     </>
   );
 };
