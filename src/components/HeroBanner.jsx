d7d19e462a7ac0c0ff0b01aa0ac15988662c857c
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-12 14:27:44 +0000

    feat: add styles for HeroBanner component

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,10 +1,25 @@
+import styled from "styled-components";
+
+const BannerWrapper = styled.div`
+  height: calc(55vh - 80px);
+`;
+
+const BannerImage = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
 const HeroBanner = ({ image, description }) => {
   if (!image) return null;
+
   return (
     <>
-      <div className="hero-banner-container" data-testid="hero-banner">
-        <img src={image} alt={description} />
-      </div>
+      {
+        <BannerWrapper data-testid="hero-banner">
+          <BannerImage src={image} alt={description} />
+        </BannerWrapper>
+      }
     </>
   );
 };
