d01df7c7b9d235bd66f9b00c0ec9fd1d11bb77f8
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-12 11:31:23 +0000

    feat: add styles for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,95 @@
 import { Link } from "react-router";
+import styled from "styled-components";
+
+const StyledNavbar = styled.nav`
+  height: ${(props) => props.theme.spacing.space80};
+  display: grid;
+  grid-template-columns: min-content 1fr;
+  grid-template-rows: 1fr;
+  align-items: center;
+  background-color: ${(props) => props.theme.colours.white};
+  color: ${(props) => props.theme.colours.lightblack};
+  padding-left: ${(props) => props.theme.spacing.space24};
+`;
+
+const LogoWrapper = styled.div`
+  height: inherit;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  font-family: ${(props) => props.theme.fonts.main};
+  font-weight: ${(props) => props.theme.weights.black};
+  font-size: 2rem;
+  text-transform: lowercase;
+`;
+
+const NavLinksList = styled.ul`
+  height: inherit;
+  display: flex;
+`;
+
+const NavLinksListItem = styled.li`
+  height: inherit;
+`;
+
+const NavLinks = styled.div`
+  height: inherit;
+  display: flex;
+  justify-self: end;
+`;
+
+const StyledLogoLink = styled(Link)`
+  height: inherit;
+  display: flex;
+  align-items: center;
+  font-family: ${(props) => props.theme.fonts.main};
+  font-weight: ${(props) => props.theme.weights.bold};
+  text-decoration: none;
+
+  &:hover {
+    color: ${(props) => props.theme.colours.green};
+  }
+`;
+
+const StyledNavLink = styled(Link)`
+  height: inherit;
+  display: flex;
+  align-items: center;
+  font-family: ${(props) => props.theme.fonts.main};
+  font-weight: ${(props) => props.theme.weights.bold};
+  padding: 0 ${(props) => props.theme.spacing.space40};
+  text-decoration: none;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colours.green};
+  }
+`;
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="logo">
-        <img src={null} alt="logo" />
-      </div>
-      <div className="nav-links">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <StyledNavbar>
+      <LogoWrapper>
+        <StyledLogoLink to="/">
+          <Logo>Multiproducts</Logo>
+        </StyledLogoLink>
+      </LogoWrapper>
+      <NavLinks>
+        <NavLinksList>
+          <NavLinksListItem>
+            <StyledNavLink to="/">Home</StyledNavLink>
+          </NavLinksListItem>
+          <NavLinksListItem>
+            <StyledNavLink to="/shop">Shop</StyledNavLink>
+          </NavLinksListItem>
+          <NavLinksListItem>
+            <StyledNavLink to="/cart">Cart</StyledNavLink>
+          </NavLinksListItem>
+        </NavLinksList>
+      </NavLinks>
+    </StyledNavbar>
   );
 };
 
