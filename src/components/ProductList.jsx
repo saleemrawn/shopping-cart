11436c919a96b5996572b5ef508b65e5a4e9b244
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-12 17:50:31 +0000

    feat: add styles for ProductList component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,11 +1,26 @@
+import styled from "styled-components";
 import ProductCard from "./ProductCard";
+
+const StyledProductList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-template-rows: repeat(2, 1fr);
+  gap: ${(props) => props.theme.spacing.space40} ${(props) => props.theme.spacing.space16};
+  margin: 0 ${(props) => props.theme.spacing.space24};
+`;
+
+const Title = styled.h2`
+  font-weight: ${(props) => props.theme.weights.black};
+  margin: ${(props) => props.theme.spacing.space40} 0;
+  text-align: center;
+`;
 
 const ProductList = ({ title, products, page }) => {
   return (
     <>
-      {title && <h2 id="product-list-heading">{title}</h2>}
+      {title && <Title>{title}</Title>}
       {products.length > 0 ? (
-        <ul aria-labelledby="product-list-heading">
+        <StyledProductList aria-labelledby="product-list-heading">
           {products.map((product) => {
             return (
               <li key={product.id}>
@@ -20,7 +35,7 @@ const ProductList = ({ title, products, page }) => {
               </li>
             );
           })}
-        </ul>
+        </StyledProductList>
       ) : (
         <div>No products</div>
       )}
