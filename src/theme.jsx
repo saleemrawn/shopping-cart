0c8012fd59b8b50f73f02a97530220dc7d155bc5
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-12 10:48:02 +0000

    feat: add styled-components theme with tokens

---
+++ src/theme.jsx
@@ -0,0 +1,41 @@
+const theme = {
+  fonts: {
+    main: `Inter, sans-serif`,
+  },
+
+  weights: {
+    light: 300,
+    regular: 400,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+
+  colours: {
+    green: "#49cb52",
+    mint: "#95e1c4",
+    teal: "#6dd5c4",
+    lightblack: "#2d2d2d",
+    charcoal: "#525050",
+    white: "#f1f1f1",
+  },
+
+  spacing: {
+    space2: "0.125rem",
+    space4: "0.25rem",
+    space8: "0.5rem",
+    space12: "0.75rem",
+    space16: "1rem",
+    space24: "1.5rem",
+    space32: "2rem",
+    space40: "2.5rem",
+    space48: "3rem",
+    space64: "4rem",
+    space80: "5rem",
+    space96: "6rem",
+    space160: "10rem",
+  },
+};
+
+export default theme;
