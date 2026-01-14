7c1ba758349b2260bd123112280f003a01d684c9
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-14 14:22:01 +0000

    feat: add Title component for page headings

---
+++ src/components/Title.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledTitle = styled.h1`
+  font-weight: ${(props) => props.theme.weights.black};
+  font-size: ${(props) => props.theme.headings[props.as]};
+`;
+
+const Title = ({ className, level = 1, children, ...props }) => {
+  return (
+    <StyledTitle as={`h${level}`} className={className} {...props}>
+      {children}
+    </StyledTitle>
+  );
+};
+
+export default Title;
