d07cc385c926eefc99eb293de42722d18201dae7
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-14 10:53:45 +0000

    feat: update style values to adapt based on display grid/list
    - add display prop to relevant style components

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,7 +5,8 @@ import styled from "styled-components";
 
 const ProductCardWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) => (props.display === "grid" ? "column" : "row")};
+  align-items: ${(props) => (props.display === "grid" ? "none" : "center")};
   gap: ${(props) => props.theme.spacing.space24};
   box-shadow: rgba(0, 0, 0, 0.1) 0rem 0.25rem 0.375rem -0.0625rem, rgba(0, 0, 0, 0.06) 0rem 0.125rem 0.25rem -0.0625rem;
 `;
@@ -17,13 +18,23 @@ const ProductImage = styled.img`
   object-fit: contain;
   background-color: ${(props) => props.theme.colours.grey200};
   padding: ${(props) => props.theme.spacing.space24};
+  max-width: ${(props) => (props.display === "list" ? "13.75rem" : null)};
+`;
+
+const ProductMainWrapper = styled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.display === "grid" ? "column" : "row")};
+  align-items: ${(props) => (props.display === "grid" ? "unset" : "center")};
+  justify-content: space-between;
+  gap: 1.5rem;
+  width: 100%;
+  padding: ${(props) => (props.display === "grid" ? props.theme.spacing.space24 : `0 ${props.theme.spacing.space24} 0 0`)};
 `;
 
 const ProductInfo = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${(props) => props.theme.spacing.space8};
-  padding: 0 ${(props) => props.theme.spacing.space24};
 `;
 
 const ProductName = styled.h3`
@@ -43,9 +54,8 @@ const ProductPrice = styled.div`
 
 const ProductCardControls = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) => (props.display === "grid" ? "column" : "row")};
   gap: 1.5rem;
-  padding: 0 ${(props) => props.theme.spacing.space24} ${(props) => props.theme.spacing.space24} ${(props) => props.theme.spacing.space24};
 `;
 
 const ProductQuantity = styled.div`
@@ -78,7 +88,7 @@ const Textbox = styled.input`
   border: 0.0625rem solid ${(props) => props.theme.colours.grey400};
 `;
 
-const ProductCard = ({ id, imgUrl, name, description, price, page = null }) => {
+const ProductCard = ({ id, imgUrl, name, description, price, page = null, display }) => {
   const [quantity, setQuantity] = useState(0);
   const { addToCart, removeFromCart } = useCart();
 
@@ -113,39 +123,40 @@ const ProductCard = ({ id, imgUrl, name, description, price, page = null }) => {
 
   return (
     <>
-      <ProductCardWrapper>
-        <ProductImage src={imgUrl} alt={description} />
-        <ProductInfo>
-          <ProductName>{name}</ProductName>
-          <ProductPrice>£{price}</ProductPrice>
-        </ProductInfo>
-
-        <ProductCardControls>
-          <ProductQuantity>
-            <Button type="button" aria-label={`decrease quantity for ${name}`} onClick={handleDecreaseQuantity}>
-              <Minus />
-            </Button>
-            <label id={`${id}-quantity`} hidden>
-              Quantity for {name}
-            </label>
-            <Textbox type="number" value={quantity} aria-labelledby={`${id}-quantity`} onChange={handleQuantityChange} />
-            <Button type="button" aria-label={`increase quantity for ${name}`} onClick={handleIncreaseQuantity}>
-              <Plus />
-            </Button>
-          </ProductQuantity>
-          {page === "cart" ? (
-            <Button type="button" aria-label={`remove ${name} from cart`} onClick={() => removeFromCart(id)}>
-              Remove
-            </Button>
-          ) : (
-            <Button
-              type="button"
-              aria-label={`add ${name} to cart`}
-              onClick={() => addToCart({ id: id, image: imgUrl, title: name, description: description, price: price })}>
-              <ShoppingCart size={20} /> Add to Cart
-            </Button>
-          )}
-        </ProductCardControls>
+      <ProductCardWrapper display={display}>
+        <ProductImage src={imgUrl} alt={description} display={display} />
+        <ProductMainWrapper display={display}>
+          <ProductInfo>
+            <ProductName>{name}</ProductName>
+            <ProductPrice>£{price}</ProductPrice>
+          </ProductInfo>
+          <ProductCardControls display={display}>
+            <ProductQuantity>
+              <Button type="button" aria-label={`decrease quantity for ${name}`} onClick={handleDecreaseQuantity}>
+                <Minus />
+              </Button>
+              <label id={`${id}-quantity`} hidden>
+                Quantity for {name}
+              </label>
+              <Textbox type="number" value={quantity} aria-labelledby={`${id}-quantity`} onChange={handleQuantityChange} />
+              <Button type="button" aria-label={`increase quantity for ${name}`} onClick={handleIncreaseQuantity}>
+                <Plus />
+              </Button>
+            </ProductQuantity>
+            {page === "cart" ? (
+              <Button type="button" aria-label={`remove ${name} from cart`} onClick={() => removeFromCart(id)}>
+                Remove
+              </Button>
+            ) : (
+              <Button
+                type="button"
+                aria-label={`add ${name} to cart`}
+                onClick={() => addToCart({ id: id, image: imgUrl, title: name, description: description, price: price })}>
+                <ShoppingCart size={20} /> Add to Cart
+              </Button>
+            )}
+          </ProductCardControls>
+        </ProductMainWrapper>
       </ProductCardWrapper>
     </>
   );
