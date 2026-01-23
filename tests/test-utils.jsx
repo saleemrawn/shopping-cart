5f78246e22089b3586af896ba54717247f7a1798
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-23 13:01:29 +0000

    test: add ThemeProvider to renderWithProviders for component rendering

--- tests/test-utils.jsx
+++ tests/test-utils.jsx
@@ -1,10 +1,14 @@
+import theme from "../src/theme";
 import { MemoryRouter } from "react-router";
 import { CartProvider } from "../src/CartProvider";
+import { ThemeProvider } from "styled-components";
 
 export const renderWithProviders = (ui, { cart = {} } = {}) => {
   return (
     <MemoryRouter>
-      <CartProvider value={cart}>{ui}</CartProvider>
+      <ThemeProvider theme={theme}>
+        <CartProvider value={cart}>{ui}</CartProvider>
+      </ThemeProvider>
     </MemoryRouter>
   );
 };
