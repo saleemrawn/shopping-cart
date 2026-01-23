137e55c76c6160a078c375f8aa684db85bf7de2c
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-23 12:58:35 +0000

    feat: add aria-label for hamburger button for screen readers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -214,7 +214,7 @@ const Navbar = () => {
 
       {window.innerWidth < 992 ? (
         <HamburgerWrapper>
-          <HamburgerButton onClick={handleNavToggle}>
+          <HamburgerButton onClick={handleNavToggle} aria-label="navigation menu">
             <Menu strokeWidth={3} />
           </HamburgerButton>
         </HamburgerWrapper>
