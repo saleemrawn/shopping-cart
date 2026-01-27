44f090209b93d4b50d931c4b5133822694a774df
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-27 12:10:24 +0000

    style: update error message font-weight & size, margin-top

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -4,8 +4,9 @@ const Message = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
+  font-size: ${(props) => props.theme.headings.mobile.h5};
   font-weight: ${(props) => props.theme.weights.bold};
-  margin-top: ${(props) => props.theme.spacing.space8};
+  margin-top: ${(props) => props.theme.spacing.space40};
 `;
 
 const ErrorMessage = ({ children }) => {
