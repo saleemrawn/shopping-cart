5e88a5baeb96ad7fac520358ad56edaf2b994847
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-01-28 19:52:32 +0000

    refactor: update renderWithProviders with Products, Cart contexts

--- tests/test-utils.jsx
+++ tests/test-utils.jsx
@@ -1,16 +1,21 @@
 import theme from "../src/theme";
 import { MemoryRouter } from "react-router";
-import { CartProvider } from "../src/providers/CartProvider";
+//import { CartProvider } from "../src/providers/CartProvider";
 import { ThemeProvider } from "styled-components";
-import { ProductsProvider } from "../src/providers/ProductsProvider";
+//import { ProductsProvider } from "../src/providers/ProductsProvider";
+import { CartContext } from "../src/components/App";
+import { ProductsContext } from "../src/components/App";
 
-export const renderWithProviders = (ui, { cart = { cartItems: [], addToCart: vi.fn(), removeFromCart: vi.fn() }, products = {} } = {}) => {
+export const renderWithProviders = (
+  ui,
+  { cart = { cartItems: [], addToCart: vi.fn(), removeFromCart: vi.fn() }, products = { products: [], error: false, loading: false } } = {},
+) => {
   return (
     <MemoryRouter>
       <ThemeProvider theme={theme}>
-        <ProductsProvider value={products}>
-          <CartProvider value={cart}>{ui}</CartProvider>
-        </ProductsProvider>
+        <ProductsContext value={products}>
+          <CartContext value={cart}>{ui}</CartContext>
+        </ProductsContext>
       </ThemeProvider>
     </MemoryRouter>
   );
